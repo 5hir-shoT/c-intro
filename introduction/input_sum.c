19528ccd0afe84e45b863b7a491d8aa92b25b91c
5hir-shoT <shirshotarafdar@gmail.com>
2025-11-21 18:05:57 +0530

    Finding sum via input

---
+++ introduction/input_sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+    {
+        int x,y,sum;
+        printf("Value 1:");
+        scanf("%d", &x);
+        printf("Value 2:");
+        scanf("%d", &y);
+        sum=x+y;
+        printf("Sum is: %d",sum);
+        return 0;
+    }
+
+/*
+
+Value 1:1
+Value 2:2
+Sum is: 3
+
+*/
